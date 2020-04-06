db482d77a303566176ba0123d4bce06481c4addd
Artem1989-dot <artemmarinichenko@ukr.net>
2020-04-06 12:30:47 +0300

    Updates

---
+++ precache-manifest.629e5ea34377decf75406a17c76fec72.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5040f40d7ae2247ec534a8549e30e31d",
+    "url": "/todo-on-react/index.html"
+  },
+  {
+    "revision": "9784dab6ed2b520e8fbb",
+    "url": "/todo-on-react/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "9c1d3b05c6ecb6a96bc2",
+    "url": "/todo-on-react/static/css/main.30da0361.chunk.css"
+  },
+  {
+    "revision": "9784dab6ed2b520e8fbb",
+    "url": "/todo-on-react/static/js/2.73613bb3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/todo-on-react/static/js/2.73613bb3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c1d3b05c6ecb6a96bc2",
+    "url": "/todo-on-react/static/js/main.ef0ec07f.chunk.js"
+  },
+  {
+    "revision": "d4fec445418451a4c28f",
+    "url": "/todo-on-react/static/js/runtime-main.f7866e3e.js"
+  }
+]);
